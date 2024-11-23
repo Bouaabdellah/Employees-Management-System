eed4105609180355e58dead783edaa9c7b70880d
Beloued Bouabdellah <bouaabd10@gmail.com>
2024-11-23 14:30:08 +0100

    add app on vercel to cors white list

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,17 +1,21 @@
 const server_port = process.env.port;
 const react_port = process.env.react_port;
-const whiteList = [`http://127.0.0.1:${server_port}`,`http://127.0.0.1:${react_port}`,
-    `http://localhost:${server_port}`,`http://localhost:${react_port}`,'http://www.employees-mg.com']
+const whiteList = [
+  `http://127.0.0.1:${server_port}`,
+  `http://127.0.0.1:${react_port}`,
+  `http://localhost:${server_port}`,
+  `http://localhost:${react_port}`,
+  'https://employees-management-system-alpha.vercel.app',
+];
 
 const corsOptions = {
-    origin : (origin,callback) => {
-        if (!origin || whiteList.indexOf(origin) !== -1)
-            callback(null,true);  // null it mean that there is no error
-        else
-            callback(new Error('not allowed by CORS'));
-    },
-    optionsSuccessStatus : 200,
-    credentials: true
-}
+  origin: (origin, callback) => {
+    if (!origin || whiteList.indexOf(origin) !== -1)
+      callback(null, true); // null it mean that there is no error
+    else callback(new Error('not allowed by CORS'));
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
 export default corsOptions;
